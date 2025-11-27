31d47e82f28b53f0d267e592267c59514b236291
MARIA BAÑULS RIBES <m.banulsribes@edu.gva.es>
2025-11-27 12:23:29 +0100

    Nota CEED

--- src/main/java/gvaCEED/DibujarLetraU.java
+++ src/main/java/gvaCEED/DibujarLetraU.java
@@ -4,8 +4,19 @@ public class DibujarLetraU {
         int filas = 7;
         int columnas = 5;
 
+        /*  matriz {
+                    {- - - - -},
+                    {- - - - -},
+                    {- - - - -},
+                    {- - - - -},
+                    {- - - - -},
+                    {- - - - -},
+                    {- - - - -}
+            } */
+
         // Paso 1: Crear la matriz de caracteres
         char[][] matriz = new char[filas][columnas];
+
 
         // Paso 2: Rellenar la matriz con espacios
         for (int i = 0; i < filas; i++) {
@@ -18,9 +29,19 @@ public class DibujarLetraU {
         // COMPLETA AQUÍ: usa bucles para poner '*' en la primera y última columna
         // excepto en la última fila
         // Ejemplo: matriz[i][0] = '*'; matriz[i][columnas - 1] = '*';
+        for (int i = 0; i < filas - 1; i++){ //excluye la ultima fila
+            matriz[i][0] = '*'; //para poner * en toda la columna 0 
+            matriz[i][columnas-1] = '*'; //para poner * en toda la columna 5
+        }
+
 
         // Paso 4: Dibujar la base de la U (última fila)
         // COMPLETA AQUÍ: pon '*' en las columnas centrales de la última fila
+        for (int i = filas - 1; i < filas; i++) {
+            matriz[i][1] = '*';
+            matriz[i][2] = '*';
+            matriz[i][3] = '*';
+        }
 
         // Paso 5: Mostrar la matriz en consola
         for (int i = 0; i < filas; i++) {
